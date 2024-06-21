bbbb9b6b3ffd266c4885961461f6a37270aa2fd3
Jan Marchel <123368854+jmarchel7bulls@users.noreply.github.com>
2024-06-21 14:26:22 +0200

    New version of onm bootstrap (#3)
    * change installation script of onm

--- installation-scripts-onm/WORKER_PRE_INSTALL_SCRIPT.sh
+++ installation-scripts-onm/WORKER_PRE_INSTALL_SCRIPT.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Worker pre-install script"
 sudo hostnamectl set-hostname "$variables_PA_JOB_NAME"
-sudo -H -u ubuntu bash -c 'https://raw.githubusercontent.com/eu-nebulous/overlay-network-manager/main/network-manager/bootstrap-agent-scripts/onm/onm-bootstrap.sh && chmod +x onm-bootstrap.sh'
+sudo -H -u ubuntu bash -c 'wget https://raw.githubusercontent.com/eu-nebulous/overlay-network-manager/main/network-manager/bootstrap-agent-scripts/onm/onm-bootstrap.sh && chmod +x onm-bootstrap.sh'
 sudo -H -u ubuntu bash -c "./onm-bootstrap.sh 'CREATE' $APPLICATION_ID $ONM_URL $PUBLIC_IP $SSH_PORT";
 
 WIREGUARD_VPN_IP=`ip a | grep wg | grep inet | awk '{print $2}' | cut -d'/' -f1`;
