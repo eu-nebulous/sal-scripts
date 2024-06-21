3a863ebccac92e75935de2922f17f7697587b143
Jan Marchel <123368854+jmarchel7bulls@users.noreply.github.com>
2024-06-21 14:24:27 +0200

    change installation script of onm (#2)
    * change installation script of onm

--- installation-scripts-onm/MASTER_PRE_INSTALL_SCRIPT.sh
+++ installation-scripts-onm/MASTER_PRE_INSTALL_SCRIPT.sh
@@ -2,8 +2,8 @@
 echo "Master pre-install script\n"
 
 sudo hostnamectl set-hostname "$variables_PA_JOB_NAME"
-sudo -H -u ubuntu bash -c 'wget https://opendev.org/nebulous/sal-scripts/raw/branch/master/onm/nm-bootstrap-script.sh && chmod +x nm-bootstrap-script.sh'
-sudo -H -u ubuntu bash -c "./nm-bootstrap-script.sh 'CREATE' 'MASTER' $APPLICATION_ID $ONM_IP $PUBLIC_IP $SSH_PORT";
+sudo -H -u ubuntu bash -c 'wget https://raw.githubusercontent.com/eu-nebulous/overlay-network-manager/main/network-manager/bootstrap-agent-scripts/onm/onm-bootstrap.sh && chmod +x onm-bootstrap.sh'
+sudo -H -u ubuntu bash -c "./onm-bootstrap.sh 'CREATE' $APPLICATION_ID $ONM_URL $PUBLIC_IP $SSH_PORT";
 echo ""
 echo ""
 sleep 60
--- installation-scripts-onm/WORKER_PRE_INSTALL_SCRIPT.sh
+++ installation-scripts-onm/WORKER_PRE_INSTALL_SCRIPT.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Worker pre-install script"
 sudo hostnamectl set-hostname "$variables_PA_JOB_NAME"
-sudo -H -u ubuntu bash -c 'wget https://opendev.org/nebulous/sal-scripts/raw/branch/master/onm/nm-bootstrap-script.sh && chmod +x nm-bootstrap-script.sh'
-sudo -H -u ubuntu bash -c "./nm-bootstrap-script.sh 'CREATE' 'WORKER' $APPLICATION_ID $ONM_IP $PUBLIC_IP $SSH_PORT";
+sudo -H -u ubuntu bash -c 'https://raw.githubusercontent.com/eu-nebulous/overlay-network-manager/main/network-manager/bootstrap-agent-scripts/onm/onm-bootstrap.sh && chmod +x onm-bootstrap.sh'
+sudo -H -u ubuntu bash -c "./onm-bootstrap.sh 'CREATE' $APPLICATION_ID $ONM_URL $PUBLIC_IP $SSH_PORT";
 
 WIREGUARD_VPN_IP=`ip a | grep wg | grep inet | awk '{print $2}' | cut -d'/' -f1`;
 echo "WIREGUARD_VPN_IP= $WIREGUARD_VPN_IP";
