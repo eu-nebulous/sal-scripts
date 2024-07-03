97b0748208d486d1bb84963274b305ede0de1fae
Jan Marchel <123368854+jmarchel7bulls@users.noreply.github.com>
2024-07-03 15:45:13 +0200

    Change source from opendev to github for install-kube-u22 (#11)

--- installation-scripts/MASTER_INSTALL_SCRIPT.sh
+++ installation-scripts/MASTER_INSTALL_SCRIPT.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Master install script"
 
-wget https://opendev.org/nebulous/sal-scripts/raw/branch/master/k8s/install-kube-u22.sh && chmod +x ./install-kube-u22.sh && ./install-kube-u22.sh
+wget https://raw.githubusercontent.com/eu-nebulous/sal-scripts/main/k8s/install-kube-u22-wg.sh && chmod +x ./install-kube-u22-wg.sh && ./install-kube-u22-wg.sh
 
 echo "Installing Helm..."
 sudo -H -u ubuntu bash -c ' curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 && chmod 700 get_helm.sh && ./get_helm.sh'
