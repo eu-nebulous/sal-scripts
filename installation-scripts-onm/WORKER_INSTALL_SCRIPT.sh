f14e2677c29c26ee3e71367cc66584c306da9f27
jmarchel7bulls <jmarchel@7bulls.com>
2024-07-03 15:44:54 +0200

    Change source from opendev to github for install-kube-u22

--- installation-scripts-onm/WORKER_INSTALL_SCRIPT.sh
+++ installation-scripts-onm/WORKER_INSTALL_SCRIPT.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Worker install script"
-wget https://opendev.org/nebulous/sal-scripts/raw/branch/master/k8s/install-kube-u22-wg.sh && chmod +x ./install-kube-u22-wg.sh && ./install-kube-u22-wg.sh
+wget https://raw.githubusercontent.com/eu-nebulous/sal-scripts/main/k8s/install-kube-u22-wg.sh && chmod +x ./install-kube-u22-wg.sh && ./install-kube-u22-wg.sh
