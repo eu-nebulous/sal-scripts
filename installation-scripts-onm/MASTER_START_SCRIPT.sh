17ceba20242a640b5f16e88f94b334c4eeadd7b6
robert-sanfeliu <robert.sanfeliu@eurecat.org>
2024-12-02 15:22:43 +0100

    Update MASTER_START_SCRIPT.sh
    delete secret docker-registry before creating it

--- installation-scripts-onm/MASTER_START_SCRIPT.sh
+++ installation-scripts-onm/MASTER_START_SCRIPT.sh
@@ -22,6 +22,7 @@ $dau bash -c 'helm repo add netdata https://netdata.github.io/helmchart/'
 $dau bash -c 'helm repo update'
 
 echo "Login to docker registry"
+$dau bash -c "kubectl delete secret docker-registry regcred --ignore-not-found"
 $dau bash -c "kubectl create secret docker-registry regcred --docker-server=$PRIVATE_DOCKER_REGISTRY_SERVER --docker-username=$PRIVATE_DOCKER_REGISTRY_USERNAME --docker-password=$PRIVATE_DOCKER_REGISTRY_PASSWORD --docker-email=$PRIVATE_DOCKER_REGISTRY_EMAIL"
 
 echo "Starting EMS"
