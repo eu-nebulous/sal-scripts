024c49d953ebc4470582b31a908551ed4e183694
robert-sanfeliu <robert.sanfeliu@eurecat.org>
2024-12-02 14:31:40 +0100

    Update MASTER_PRE_INSTALL_SCRIPT.sh
    Specify destination file for wget command to avoid versioning

--- installation-scripts-onm/MASTER_PRE_INSTALL_SCRIPT.sh
+++ installation-scripts-onm/MASTER_PRE_INSTALL_SCRIPT.sh
@@ -9,7 +9,7 @@ K3S_DEP_PATH=$HOME/k3s
 mkdir -p $K3S_DEP_PATH
 
 echo "Setting Wireguard Interface\n"
-sudo -H -u ubuntu bash -c 'wget https://raw.githubusercontent.com/eu-nebulous/overlay-network-manager/main/network-manager/bootstrap-agent-scripts/onm/onm-bootstrap.sh && chmod +x onm-bootstrap.sh'
+sudo -H -u ubuntu bash -c 'wget https://raw.githubusercontent.com/eu-nebulous/overlay-network-manager/main/network-manager/bootstrap-agent-scripts/onm/onm-bootstrap.sh -O onm-bootstrap.sh  && chmod +x onm-bootstrap.sh'
 sudo -H -u ubuntu bash -c "./onm-bootstrap.sh 'CREATE' $APPLICATION_ID $ONM_URL $PUBLIC_IP $SSH_PORT";
 echo ""
 echo ""
@@ -19,4 +19,4 @@ WIREGUARD_VPN_IP=`ip a | grep wg | grep inet | awk '{print $2}' | cut -d'/' -f1`
 echo "WIREGUARD_VPN_IP= $WIREGUARD_VPN_IP";
 
 echo "Executing k3s-preinstall script\n"
-sudo -H -u ubuntu bash -c "wget -P ${K3S_DEP_PATH} https://raw.githubusercontent.com/eu-nebulous/sal-scripts/dev/k3s/preinstall-kube-k3s-u22.sh && chmod +x $K3S_DEP_PATH/preinstall-kube-k3s-u22.sh && $K3S_DEP_PATH/preinstall-kube-k3s-u22.sh"
+sudo -H -u ubuntu bash -c "wget https://raw.githubusercontent.com/eu-nebulous/sal-scripts/dev/k3s/preinstall-kube-k3s-u22.sh -O ${K3S_DEP_PATH}/preinstall-kube-k3s-u22.sh  && chmod +x $K3S_DEP_PATH/preinstall-kube-k3s-u22.sh && $K3S_DEP_PATH/preinstall-kube-k3s-u22.sh"
