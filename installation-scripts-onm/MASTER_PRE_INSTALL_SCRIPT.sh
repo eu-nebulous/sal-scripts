2ee4fa24bf38e3682f3008d61746c8583f1f468b
robert-sanfeliu <robert.sanfeliu@eurecat.org>
2024-12-02 14:44:09 +0100

    Update MASTER_PRE_INSTALL_SCRIPT.sh
    Wait for WIREGUARD_IP

--- installation-scripts-onm/MASTER_PRE_INSTALL_SCRIPT.sh
+++ installation-scripts-onm/MASTER_PRE_INSTALL_SCRIPT.sh
@@ -15,8 +15,16 @@ echo ""
 echo ""
 sleep 60
 
-WIREGUARD_VPN_IP=`ip a | grep wg | grep inet | awk '{print $2}' | cut -d'/' -f1`;
-echo "WIREGUARD_VPN_IP= $WIREGUARD_VPN_IP";
+while true; do
+    WIREGUARD_VPN_IP=$(ip a | grep wg | grep inet | awk '{print $2}' | cut -d'/' -f1)
+    if [[ -n "$WIREGUARD_VPN_IP" ]]; then
+        log_print INFO "WIREGUARD_VPN_IP is set to $WIREGUARD_VPN_IP"
+        break
+    fi
+    log_print INFO "Waiting for WIREGUARD_VPN_IP to be set..."
+    sleep 2
+done
+
 
 echo "Executing k3s-preinstall script\n"
 sudo -H -u ubuntu bash -c "wget https://raw.githubusercontent.com/eu-nebulous/sal-scripts/dev/k3s/preinstall-kube-k3s-u22.sh -O ${K3S_DEP_PATH}/preinstall-kube-k3s-u22.sh  && chmod +x $K3S_DEP_PATH/preinstall-kube-k3s-u22.sh && $K3S_DEP_PATH/preinstall-kube-k3s-u22.sh"
