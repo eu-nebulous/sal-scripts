4c158ca5869fecc669d8197b0713ce63c52d6fa2
robert.sanfeliu <robert.sanfeliu@eurecat.org>
2025-03-31 10:55:31 +0200

    Added loging to join command

--- installation-scripts-onm/WORKER_START_SCRIPT.sh
+++ installation-scripts-onm/WORKER_START_SCRIPT.sh
@@ -2,6 +2,6 @@
 echo "Worker start script"
 if [[ "$CONTAINERIZATION_FLAVOR" != "k3s" ]]; then
     sudo kubeadm reset --force
-    echo $variables_kubeCommand
+    echo "Join command: $variables_kubeCommand"
     sudo $variables_kubeCommand
 fi
