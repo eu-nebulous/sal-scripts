3a392209e9e6af7cef29fb78c3428b0c135d3b6e
robert.sanfeliu <robert.sanfeliu@eurecat.org>
2025-06-03 15:42:03 +0200

    adding to woker  "sudo modprobe br_netfilter" to fix error “/proc/sys/net/bridge/bridge-nf-call-iptables does not exist”

--- installation-scripts-onm/WORKER_START_SCRIPT.sh
+++ installation-scripts-onm/WORKER_START_SCRIPT.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "Worker start script"
+echo "modprobe br_netfilter"
+sudo modprobe br_netfilter
+echo "modprobe br_netfilter done"
+
 sudo kubeadm reset --force
 echo $variables_kubeCommand
 sudo $variables_kubeCommand
