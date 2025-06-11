edd38d0fd955035ab42dc9bb226557a851743fe6
robert.sanfeliu <robert.sanfeliu@eurecat.org>
2025-06-11 09:03:47 +0200

    modprobe br_netfilter on worker

--- installation-scripts-onm/WORKER_START_SCRIPT.sh
+++ installation-scripts-onm/WORKER_START_SCRIPT.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo "Worker start script"
+
+echo "modprobe br_netfilter"
+sudo modprobe br_netfilter
+echo "modprobe br_netfilter done"
+
 if [[ "$CONTAINERIZATION_FLAVOR" != "k3s" ]]; then
     sudo kubeadm reset --force
     echo "Join command: $variables_kubeCommand"
