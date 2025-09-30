db391860a4c85dc110ea69d47b9b634cd6edf9b7
robert.sanfeliu <robert.sanfeliu@eurecat.org>
2025-09-30 11:17:19 +0200

    ems-server install use $NEBULOUS_SCRIPTS_BRANCH env var

--- installation-scripts-onm/MASTER_START_SCRIPT.sh
+++ installation-scripts-onm/MASTER_START_SCRIPT.sh
@@ -203,7 +203,7 @@ $dau bash -c 'helm install ems nebulous/ems-server \
   --set tolerations[0].effect="NoSchedule" \
   --set app_uuid=$APPLICATION_ID \
   --set broker_address=$BROKER_ADDRESS \
-  --set image.tag="main" \
+  --set image.tag=$NEBULOUS_SCRIPTS_BRANCH \
   --set client.image.tag="ems-client-main" \
   --set broker_port=$BROKER_PORT'
 
